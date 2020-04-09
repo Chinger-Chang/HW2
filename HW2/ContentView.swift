64c5ce469ebf742ff674ae190c474c6090d0b62d
andy <andy870622@gmail.com>
2020-04-09 21:06:20 +0800

    Initial Commit

---
+++ HW2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW2
+//
+//  Created by User15 on 2020/4/9.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
